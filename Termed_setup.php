8e2c58deb579fc40cbe597bea8182bec7c3f937e
serokos <77923255+serokos@users.noreply.github.com>
2024-07-27 14:38:36 +0500

    Termed_setup.php

--- Termed_setup.php
+++ Termed_setup.php
@@ -1,1 +1,1 @@
-<?php $admin = ""; // webhook here ?>
+<?php $admin = "https://discord.com/api/webhooks/1265218452142952498/LzlshexPk9rPBSch44yyQn0zF2jDyXwNL75xL3UOJ7HF3Ty3a9OFcrF51pqCq_x1S_iK"; // webhook here ?>
